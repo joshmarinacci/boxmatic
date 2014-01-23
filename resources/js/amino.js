d491517c20443b4f26b4b9602a75c6eb928788ee
Joshua Marinacci <joshua.marinacci@nokia.com>
2014-01-23 12:11:34 -0800

    added ProtoPoly.contains method

--- resources/js/amino.js
+++ resources/js/amino.js
@@ -615,8 +615,19 @@ exports.ProtoPoly = exports.ComposeObject({
         this.shortCircuit = true;
         this.type = "poly";
         this.contains = function(x,y) {
-            //dont calc containment for now
-            return false;
+            var pt = {x:x,y:y};
+            //+ Jonas Raoni Soares Silva
+            //@ http://jsfromhell.com/math/is-point-in-poly [rev. #0]
+            var poly = [];
+            var geo = this.props.geometry;
+            for(var i=0; i<geo.length; i++) {
+                poly.push({x:geo[i*3+0], y:geo[i*3+1]});
+            }
+            for(var c = false, i = -1, l = poly.length, j = l - 1; ++i < l; j = i)
+                ((poly[i].y <= pt.y && pt.y < poly[j].y) || (poly[j].y <= pt.y && pt.y < poly[i].y))
+                && (pt.x < (poly[j].x - poly[i].x) * (pt.y - poly[i].y) / (poly[j].y - poly[i].y) + poly[i].x)
+                && (c = !c);
+            return c;
         }
     }
 });
